a1b6bc014d1484a95a643d47221231a3dd60226a
ivnlsv <ivan.losev@denuo.legal>
2024-06-24 16:25:58 +0300

    Revert "Add files via upload"
    This reverts commit 6658e98f9ba3657b958b484f2851b3143c6d4bd6.

--- Ex-5-10-2.js
+++
@@ -1,7 +0,0 @@
-const inputedPromoCode = prompt("Введите промокод");
-const promoCode = "скидка";
-if (promoCode === inputedPromoCode.toLowerCase()) {
-  alert("Промокод применен");
-} else {
-  alert("Промокод не работает");
-}
--- Ex-6-6-3.js
+++
@@ -1,6 +0,0 @@
-const userInput = +prompt("Введите число");
-const arr = [];
-for (let i = 0; i <= userInput; i += 1) {
-  arr.push(i);
-}
-console.log(arr);

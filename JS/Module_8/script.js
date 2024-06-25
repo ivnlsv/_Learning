08f931c7c62e33d13a87d122a9cc82068dcf73d8
ivnlsv <78484056+ivnlsv@users.noreply.github.com>
2024-06-25 14:47:58 +0300

    Update script.js

--- JS/Module_8/script.js
+++ JS/Module_8/script.js
@@ -1,19 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const gallery = document.getElementById('gallery');
-  const serverEndpoint = 'https://dog.ceo/api/breeds/image/random/20'; // Replace with your server endpoint
 
-  // Fetch images from the server
-  fetch(serverEndpoint)
-    .then(response => response.json())
-    .then(images => {
-      images.forEach(url => {
-        const img = document.createElement('img');
-        img.src = url;
-        img.alt = "Image from server";
-        gallery.appendChild(img);
-      });
-    })
-    .catch(error => {
-      console.error('Error fetching images:', error);
-    });
-});
